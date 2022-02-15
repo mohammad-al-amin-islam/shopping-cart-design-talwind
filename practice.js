597f0dfc11813c82bae84f24e50fc0c249da9bd2
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-02-15 22:36:47 +0600

    making simple design with talwind

---
+++ practice.js
@@ -0,0 +1,22 @@
+/* const booksName =[
+    'JavaScript & JQuery: Interactive Front-End Web Development',
+    'Eloquent JavaScript: A Modern Introduction to Programming',
+    'A Smarter Way to Learn JavaScript: The new tech-assisted approach that requires half the effort',
+    'High-Performance Browser Networking',
+    ' JavaScript for Kids: A Playful Introduction to Programming'
+
+];
+let search = 'javascript';
+const newArray = [];
+for(const book of booksName){
+    if(book.toLocaleLowerCase().includes(search)){
+        newArray.push(book);
+    }
+}
+console.log(newArray); */
+
+/* const number =[34,634,64,7,734,24,644,6];
+number.sort(function(a,b){
+    return a-b;
+})
+console.log(number); */
